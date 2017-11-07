83badd2e29e520d602605d5e126043fa933d27cd
RJ Duane <git@rjd.io>
2017-11-07 13:43:59 +0000

    Add basic ticket controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,18 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+    @ticket = Ticket.find(find(params[:id]))
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+  end
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:title, :body)
+  end
 end
