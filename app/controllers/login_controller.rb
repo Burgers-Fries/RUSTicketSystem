6f784519be5ff085a45cd0875a0bd7f5c5c4da88
Luke Frazier <lfrazi18@uncc.edu>
2017-11-13 21:43:09 +0000

    Add mailer test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,5 @@
 class LoginController < ApplicationController
   def index
+    RusMailer.sample_email("lfrazi18@uncc.edu").deliver
   end
 end
--- test/mailers/previews/rus_mailer_preview.rb
+++ test/mailers/previews/rus_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/rus_mailer
 class RusMailerPreview < ActionMailer::Preview
     def rus_mail_preview
-        RusMailer.sample_email("Luke")
+        RusMailer.sample_email("lfrazi18@uncc.edu")
     end
 end
